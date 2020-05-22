2e54358a7c4796bae057b1e0530247c2102a1b32
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2020-05-22 19:58:35 +0530

    3 files modified

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ import Render1 from "./render1.js";
 import Render2 from "./render2.js";
 import Component1 from "./Component1";
 import { Provider } from "./Component";
+import axios from "axios";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      count: 0
+      count: 0,
+      posts: []
     };
     this.inputRef = React.createRef();
   }
@@ -23,12 +25,18 @@ class App extends Component {
   componentDidMount() {
     console.log("this-->", this.inputRef);
     this.inputRef.current.focus();
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => this.setState({ posts: response.data }))
+      .catch(error => console.log("err", error));
   }
 
   render() {
     console.log("parent ----->");
     return (
       <div>
+        {this.state.posts &&
+          this.state.posts.map(data => <h1 key={data.id}>{data.title}</h1>)}
         <h1>parent component</h1>
         <input type="text" ref={this.inputRef} />
         <hr />
@@ -41,7 +49,7 @@ class App extends Component {
         <Render2 />
         <hr />
         <Provider value="Jagath">
-          <Component1 name="Jagath" />
+          <Component1 />
         </Provider>
       </div>
     );
